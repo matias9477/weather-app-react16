aab5d84874e366dbf94ba2ab0bc2b8c36fc9f647
matias.turra <matias.turra@gmail.com>
2019-07-08 15:00:30 -0300

    agregados estilos en css

--- src/components/WeatherLocation/WeatherData/WeatherExtraInfo.js
+++ src/components/WeatherLocation/WeatherData/WeatherExtraInfo.js
@@ -1,9 +1,10 @@
 
 import React from 'react';
 import PropTypes from 'prop-types'
+import './styles.css';
 
 const WeatherExtraInfo = ({ humidity, wind }) => (
-    <div>
+    <div className="weatherExtraInfoCont">
         <span>{`${humidity} % - `}</span>
         <span>{`${wind} viento`}</span>
     </div>
