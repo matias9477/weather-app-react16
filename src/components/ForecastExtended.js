3b64978c6de3271f9aae3293f6fb338d1bd179e2
matias.turra <matias.turra@gmail.com>
2019-08-20 21:49:37 -0300

    Forecast Item

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -1,13 +1,32 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import './styles.css';
+import ForecastItem from './ForecastItem/';
+
+const data = {
+    temperature: 10,
+    humidity: 10,
+    weatherState: 'normal',
+    wind: 'culo'
+}
+
+const days = [
+    'Lunes','Martes', 'Miercoles', 'Jueves', 'Viernes',
+];
 
 class ForecastExtended extends Component {
+
+    renderForecastItemDays(){
+        return days.map( day => (<ForecastItem weekDay={day} hour={10} data={data}/>));
+    }
+
+
     render(){
         const city = this.props.city;
         
         return (<div>
             <h2 className="forecastTittle">Pronóstico extendido para {city}</h2>
+            {this.renderForecastItemDays()}
             </div>);
     }
 }
