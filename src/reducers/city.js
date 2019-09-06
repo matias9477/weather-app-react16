0696b959411226f7404ae593433effcfb3f1a458
matias <matias.turra@gmail.com>
2019-09-06 17:54:05 -0300

    Primer reducar

--- src/reducers/city.js
+++ src/reducers/city.js
@@ -1,9 +1,12 @@
 import {SET_CITY} from './../actions';
 
-export const city = (state, action) => {
+export const city = (state = {}, action) => {
     switch (action.type) {
         case SET_CITY:
-            return {...state, city: action.value}
-    }
-    return state;
+            return {...state, city: action.payload}    
+            // si state tiene una propiedad city, cambiala por action.payload
+            //payload es como el value, action.payload devuelve la city a cambiar
+        default:
+            return state;
+        }
 } 
