f825052263df7a94f7a84d95cea078317ff6f6f4
matias.turra <matias.turra@gmail.com>
2020-03-17 17:15:35 -0300

    comit petero

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import {combineReducers} from 'redux';
+import {createSelector} from 'reselect';
 import {city} from './city';
 import {cities, getForecastDataFromCities as _getForecastDataFromCities} from './cities';
 
@@ -8,6 +9,6 @@ export default combineReducers({
 });
 
 
-export const getCity = state => state.city;
+export const getCity = createSelector(state => state.city, city => city);
 
 export const getForecastDataFromCities = state => (_getForecastDataFromCities(state.cities, getCity(state)));
